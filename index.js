507c43cc6041c650c61f5dcbaa2269c6deff5931
David Felix <david.felix02@hotmail.com>
2020-09-16 18:20:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function updateDriverWithKeyAndValue(driverObj,key,value){
   return Object.assign({}, driverObj, { [key]: value });
 };
+
 function destructivelyUpdateDriverWithKeyAndValue(driverObj,key,value){
   driverObj[key] = value;
  
   return obj;
+};
+
+function deleteFromDriverByKey(driverObj,key){
+  delete driverObj.key
 }
 
 
